24aaecc745d9ab024f7ed8e89603737a7f93bff0
Alexey Raga <alexey@raga.name>
2012-08-09 20:58:51 +1000

    Event store and user context/audit basics

---
+++ Infrastructure/Ziggurat.Infrastructure/EventStore/EventHeaderKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ziggurat.Infrastructure.EventStore
+{
+    public static class EventHeaderKeys
+    {
+        public static string DateCreated = "DateCreated";
+        public static string MemberId = "MemberId";
+    }
+}
---
+++ Infrastructure/Ziggurat.Infrastructure/EventStore/JOEventStore.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventStore;
+
+namespace Ziggurat.Infrastructure.EventStore
+{
+    public sealed class JOEventStore : IEventStore
+    {
+        private readonly IStoreEvents _realEventStore;
+        private readonly IUserContextProvider _userContextProvider;
+
+        public JOEventStore(IStoreEvents realEventStore, IUserContextProvider userContextProvider)
+        {
+            if (realEventStore == null) throw new ArgumentNullException("realEventStore");
+            if (userContextProvider == null) throw new ArgumentNullException("userContextProvider");
+
+            _realEventStore = realEventStore;
+            _userContextProvider = userContextProvider;
+        }
+
+        public EventStream Load(Guid aggregateIdentity)
+        {
+            //TODO: When load, how to handle headers? Should we at all? I think we don't as aggregates don't (shouldn't) care...
+            throw new NotImplementedException();
+        }
+
+        public void Append(Guid aggregateIdentity, int revision, IEnumerable<object> events)
+        {
+            Append(aggregateIdentity, revision, new Guid(), events);
+        }
+
+        public void Append(Guid aggregateIdentity, int revision, Guid commitId, IEnumerable<object> events)
+        {
+            if (aggregateIdentity == null) throw new ArgumentNullException("aggregateIdentity");
+            if (events == null) return;
+
+            var userContext = _userContextProvider.GetCurrentContext();
+
+            var evtMessages = ConvertToEventMessages(events, userContext);
+
+            using (var stream = _realEventStore.OpenStream(aggregateIdentity, revision, int.MaxValue))
+            {
+                foreach (var evt in evtMessages) stream.Add(evt);
+                stream.CommitChanges(commitId);
+            }
+        }
+
+        public void Dispose()
+        {
+            _realEventStore.Dispose();
+        }
+
+        private static IEnumerable<EventMessage> ConvertToEventMessages(IEnumerable<object> events, IUserContext userContext)
+        {
+            foreach (var evt in events)
+            {
+                var msg = new EventMessage { Body = evt };
+
+                //populate message headers: extra information for audit and history reasons.
+                FillInGenericHeaders(msg);
+                FillInUserContextHeaders(msg, userContext);
+
+                yield return msg;
+            }
+        }
+
+        private static void FillInGenericHeaders(EventMessage msg)
+        {
+            msg.Headers.Add(EventHeaderKeys.DateCreated, Now.UtcTime);
+        }
+
+        private static void FillInUserContextHeaders(EventMessage msg, IUserContext userContext)
+        {
+            if (userContext == null) return;
+
+            msg.Headers.Add(EventHeaderKeys.MemberId, userContext.MemberId);
+        }
+
+    }
+}
