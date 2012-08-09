a103e8503cfbe672083124645ea6549c562f1590
ldsenow <ldsenow@gmail.com>
2012-08-09 23:19:15 +1000

    implemented InMemoryProjectionReaderWritter

---
+++ Infrastructure/Ziggurat.Infrastructure.Tests/Projections/InMemoryProjectionReaderWriterTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ziggurat.Infrastructure.Projections;
+
+namespace Ziggurat.Infrastructure.Tests.Projections
+{
+    [TestClass]
+    public class InMemoryProjectionReaderWriterTest
+    {
+        [TestMethod]
+        public void Should_be_able_add_a_view()
+        {
+            var readerWriter = new InMemoryProjectionReaderWriter<Guid, DummyView>();
+            var view = new DummyView() {
+                Id = Guid.NewGuid(),
+            };
+
+            var addedView = readerWriter.Add(view.Id, view);
+            Assert.AreEqual(view, addedView);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), 
+            "InvalidOperationException should be thrown if trying to add a view with existing key")]
+        public void Should_not_be_able_to_add_a_view_with_existing_key()
+        {
+            var readerWriter = new InMemoryProjectionReaderWriter<Guid, DummyView>();
+            var key = Guid.NewGuid();
+
+            var view1 = new DummyView() {
+                Id = key,
+                Anything = Guid.NewGuid().ToString()
+            };
+
+            var view2 = new DummyView() {
+                Id = key,
+                Anything = Guid.NewGuid().ToString()
+            };
+
+            readerWriter.Add(key, view1);
+            readerWriter.Add(key, view2);
+        }
+
+        private class DummyView
+        {
+            public Guid Id { get; set; }
+            public string Anything { get; set; }
+        }
+    }
+}
--- Infrastructure/Ziggurat.Infrastructure/Projections/InMemoryProjectionReaderWriter.cs
+++ Infrastructure/Ziggurat.Infrastructure/Projections/InMemoryProjectionReaderWriter.cs
@@ -1,27 +1,31 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace Ziggurat.Infrastructure.Projections
 {
-	public sealed class InMemoryProjectionReaderWriter<TKey, TView> : IProjectionReader<TKey, TView>, IProjectionWriter<TKey, TView>
-	{
-		public bool TryGet(TKey key, out TView view)
-		{
-			throw new NotImplementedException();
-		}
+    public sealed class InMemoryProjectionReaderWriter<TKey, TView> : IProjectionReader<TKey, TView>, IProjectionWriter<TKey, TView>
+    {
+        private static readonly ConcurrentDictionary<TKey, TView> Mappings = new ConcurrentDictionary<TKey, TView>();
 
-	    public bool Exists(TKey key)
-	    {
-	        throw new NotImplementedException();
-	    }
+        public bool TryGet(TKey key, out TView view)
+        {
+            return Mappings.TryGetValue(key, out view);
+        }
 
-	    public TView AddOrUpdate(TKey key, Func<TView> addFactory, Func<TView, TView> updateFactory)
-		{
-			throw new NotImplementedException();
-		}
+        public bool Exists(TKey key)
+        {
+            return Mappings.ContainsKey(key);
+        }
 
-		public bool TryDelete(TKey key)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public TView AddOrUpdate(TKey key, Func<TView> addFactory, Func<TView, TView> updateFactory)
+        {
+            return Mappings.AddOrUpdate(key, k => addFactory(), (k, v) => updateFactory(v));
+        }
+
+        public bool TryDelete(TKey key)
+        {
+            TView view;
+            return Mappings.TryRemove(key, out view);
+        }
+    }
 }
