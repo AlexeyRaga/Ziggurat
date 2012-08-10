6848ee25d1008464b4128fab5a68bde6ac44bec5
Alexey Raga <alexey.raga@gmail.com>
2012-08-10 20:53:17 +1000

    Project Aggregate

---
+++ Definition/Ziggurat.Definition.Domain/Project/ProjectApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Definition.Domain.Base;
+using Ziggurat.Infrastructure.EventStore;
+
+namespace Ziggurat.Definition.Domain.Project
+{
+    public sealed class ProjectApplicationService : ApplicationServiceBase<ProjectAggregate>
+    {
+        public ProjectApplicationService(IEventStore eventStore) : base(eventStore)
+        {
+
+        }
+    }
+}
---
+++ Definition/Ziggurat.Definition.Domain/Project/ProjectState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Definition.Domain.Base;
+
+namespace Ziggurat.Definition.Domain.Project
+{
+    public sealed class ProjectState : AggregateState
+    {
+    }
+}
