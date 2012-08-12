cefe7e2f09a652cb6351980ea2e17f072a32a4fb
Alexey Raga <alexey.raga@gmail.com>
2012-08-12 22:55:36 +1000

    Accepting definition commands

--- Definition/Ziggurat.Definition.Domain/FormDefinition/FormDefinitionApplicationService.cs
+++ Definition/Ziggurat.Definition.Domain/FormDefinition/FormDefinitionApplicationService.cs
@@ -16,5 +16,25 @@ namespace Ziggurat.Definition.Domain.FormDefinition
 
             Update(cmd.Id, form => form.Create(cmd.ProjectId, cmd.Id, cmd.Name, cmd.UniqueName));
         }
+
+        public void When(CreateProperty cmd)
+        {
+            if (cmd.FormId == Guid.Empty) throw new ArgumentException("Form ID is required");
+            if (String.IsNullOrWhiteSpace(cmd.Name)) throw new ArgumentException("Name is required");
+
+            Update(cmd.FormId, form => form.CreateProperty(cmd.PropertyId, cmd.Type, cmd.Name));
+        }
+
+        public void When(MakePropertyUsed cmd)
+        {
+            if (cmd.FormId == Guid.Empty) throw new ArgumentException("Form ID is required");
+            Update(cmd.FormId, form => form.MakePropertyUsed(cmd.PropertyId));
+        }
+
+        public void When(MakePropertyUnused cmd)
+        {
+            if (cmd.FormId == Guid.Empty) throw new ArgumentException("Form ID is required");
+            Update(cmd.FormId, form => form.MakePropertyUnused(cmd.PropertyId));
+        }
     }
 }
