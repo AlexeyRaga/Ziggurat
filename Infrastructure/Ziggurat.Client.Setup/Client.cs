f7485e1fb90b51c4a188cf17c2cfb784af66bc39
Alexey Raga <alexey.raga@gmail.com>
2012-08-14 17:24:48 +1000

    Simple client wired up

--- Infrastructure/Ziggurat.Client.Setup/Client.cs
+++ Infrastructure/Ziggurat.Client.Setup/Client.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using EventStore;
 using EventStore.Dispatcher;
+using Ziggurat.Infrastructure.Evel;
 using Ziggurat.Infrastructure.EventStore;
 
 namespace Ziggurat.Client.Setup
@@ -12,19 +11,52 @@ namespace Ziggurat.Client.Setup
     {
         private static readonly byte[] EncryptionKey = new byte[] { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf };
 
-        public IEventStore EventStore { get; private set; }
+		private readonly IMessageDispatcher _commandDispatcher = new ConventionalToWhenDispatcher();
+		private readonly IMessageDispatcher _eventsDispatcher = new ConventionalToWhenDispatcher();
+
+		
+		public IEventStore EventStore { get; private set; }
+
+		public static Client Create()
+		{
+			return new Client();
+		}
+
+		public void SubscribeToCommands(object handler)
+		{
+			_commandDispatcher.Subscribe(handler);
+		}
+
+		public void SubscribeToEvents(object handler)
+		{
+			_eventsDispatcher.Subscribe(handler);
+		}
+
+		private Client()
+		{
+			var eventStore = BuildEventStore(DispatchEvents);
+			EventStore = eventStore;
+		}
+
+		private void DispatchEvents(IEnumerable<Envelope> events)
+		{
+			foreach (var evt in events)
+			{
+				_eventsDispatcher.DispatchToAll(evt.Body);
+			}
+		}
 
         private static IEventStore BuildEventStore(Action<IEnumerable<Envelope>> eventsDispatcher)
         {
-            var joEventStore = WireupEventStore(eventsDispatcher);
+            var joEventStore = ConfigureEventStore(eventsDispatcher);
             return new JOEventStore(joEventStore);
         }
 
-        private static IStoreEvents WireupEventStore(Action<IEnumerable<Envelope>> eventsDispatcher)
+        private static IStoreEvents ConfigureEventStore(Action<IEnumerable<Envelope>> eventsDispatcher)
         {
             return Wireup.Init()
                .LogToOutputWindow()
-               .UsingSqlPersistence("ESTest") // Connection string is in app.config
+               .UsingSqlPersistence("EventStream") // Connection string is in app.config
                    .EnlistInAmbientTransaction() // two-phase commit
                    .InitializeStorageEngine()
                    .UsingJsonSerialization()
--- Infrastructure/Ziggurat.Infrastructure/EventStore/Envelope.cs
+++ Infrastructure/Ziggurat.Infrastructure/EventStore/Envelope.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
-using System.Runtime.Serialization;
-using System.Xml.Serialization;
 
 namespace Ziggurat.Infrastructure.EventStore
 {
