be3217fccf2a150c4bbbbb8943888a18ff236323
Alexey Raga <alexey.raga@gmail.com>
2012-08-15 19:45:21 +1000

    ProjectStructure: try 1

---
+++ Definition/Ziggurat.Definition.Domain/DomainBoundedContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Infrastructure;
+using Ziggurat.Infrastructure.EventStore;
+
+namespace Ziggurat.Definition.Domain
+{
+    public static class DomainBoundedContext
+    {
+        public static IEnumerable<object> BuildProjections()
+        {
+            yield break;
+        }
+
+        public static IEnumerable<object> BuildApplicationServices(IEventStore eventStore)
+        {
+            yield return new FormDefinition.FormDefinitionApplicationService(eventStore);
+            yield return new Project.ProjectApplicationService(eventStore);
+        }
+
+        public static IEnumerable<object> BuildEventProcessors(IBus bus)
+        {
+            yield return new ProjectStructure.ProjectStructureDomainService(bus);
+        }
+    }
+}
---
+++ Definition/Ziggurat.Definition.Domain/ProjectStructure/ProjectStructureState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Contracts;
+
+namespace Ziggurat.Definition.Domain.ProjectStructure
+{
+    public sealed class ProjectStructureState : State
+    {
+        public bool Created { get; set; }
+
+        public void When(ProjectStructureCreated evt)
+        {
+            Created = true;
+        }
+    }
+}
