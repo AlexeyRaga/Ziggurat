6c79979837d46443093950eb45e615c5bf16ec9b
Alexey Raga <alexey.raga@gmail.com>
2012-08-15 19:16:10 +1000

    Refactoring: IBus interface

---
+++ Infrastructure/Ziggurat.Infrastructure/IBus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ziggurat.Infrastructure
+{
+	public interface IBus : IDisposable
+	{
+		void SubscribeToCommands(object handler);
+		void SubscribeToEvents(object handler);
+
+        void SendCommand(object command);
+	}
+}
