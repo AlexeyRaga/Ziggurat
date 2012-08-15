e492c6224e99569d871da108a2d01f990102eefc
Alexey Raga <alexey.raga@gmail.com>
2012-08-15 23:26:18 +1000

    Project creation process

---
+++ Definition/Ziggurat.Definition.Domain/Processes/ProjectCreationProcess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Contracts;
+using Ziggurat.Infrastructure;
+
+namespace Ziggurat.Definition.Domain.Processes
+{
+    public sealed class ProjectCreationProcess
+    {
+        private readonly ICommandSender _commandSender;
+        public ProjectCreationProcess(ICommandSender commandSender)
+        {
+            _commandSender = commandSender;
+        }
+
+        public void When(ProjectCreated evt)
+        {
+            _commandSender.SendCommand(new CreateProjectStructure(evt.Id, new Guid()));
+        }
+    }
+}
---
+++ Definition/Ziggurat.Definition.Domain/ProjectStructure/ProjectStructureState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ziggurat.Definition.Domain.ProjectStructure
+{
+    public sealed class ProjectStructureState : State
+    {
+    }
+}
