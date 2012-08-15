380fbb49d84a74c71d8801e3e3d027d603915a0c
Alexey Raga <alexey.raga@gmail.com>
2012-08-15 23:24:24 +1000

    Add sender interface

--- Infrastructure/Ziggurat.Infrastructure/IBus.cs
+++ Infrastructure/Ziggurat.Infrastructure/IBus.cs
@@ -2,11 +2,14 @@
 
 namespace Ziggurat.Infrastructure
 {
+    public interface ICommandSender
+    {
+        void SendCommand(object command);
+    }
+
 	public interface IBus : IDisposable
 	{
 		void SubscribeToCommands(object handler);
 		void SubscribeToEvents(object handler);
-
-        void SendCommand(object command);
 	}
 }
