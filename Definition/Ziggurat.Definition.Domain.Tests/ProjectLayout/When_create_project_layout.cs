195c209913f2a6d5fc4c83bbb538c1441e3446b3
Alexey Raga <alexey.raga@gmail.com>
2012-08-18 15:31:22 +1000

    Project layout unit tests

---
+++ Definition/Ziggurat.Definition.Domain.Tests/ProjectLayout/When_create_project_layout.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Contracts;
+using Ziggurat.Definition.Domain.ProjectLayout;
+
+namespace Ziggurat.Definition.Domain.Tests.ProjectLayout
+{
+    [TestClass]
+    public sealed class When_create_project_layout : AggregateTest<ProjectLayoutAggregate>
+    {
+        private Guid ProjectId = Guid.NewGuid();
+        private Guid LayoutId = Guid.NewGuid();
+        
+        [TestMethod]
+        public void Should_create_aggregate()
+        {
+            When = layout => layout.CreateForProject(ProjectId, LayoutId);
+            Then = new IEvent[] {
+                new ProjectLayoutCreated(ProjectId, LayoutId)
+            };
+        }
+    }
+}
