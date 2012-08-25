a34fc40572b7ee931888a50b8d0e12ead8485bf9
Alexey Raga <alexey.raga@gmail.com>
2012-08-25 20:12:15 +1000

    Generic serializer

---
+++ Infrastructure/Ziggurat.Infrastructure/Serialization/SerializationException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ziggurat.Infrastructure.Serialization
+{
+    [Serializable]
+    public class SerializationException : Exception
+    {
+        public SerializationException() { }
+
+        public SerializationException(string message)
+            : base(message) { }
+
+        public SerializationException(string message, Exception inner)
+            : base(message, inner) { }
+
+        protected SerializationException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
