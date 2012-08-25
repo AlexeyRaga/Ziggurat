39db597d2d1c213b0f3ea85ebccbee06d1f77e89
Alexey Raga <alexey.raga@gmail.com>
2012-08-25 21:08:12 +1000

    Registration domain

---
+++ Registration/Ziggurat.Registration.Domain/Base/ApplicationServiceBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using Ziggurat.Contracts;
+using Ziggurat.Infrastructure;
+using Ziggurat.Infrastructure.EventStore;
+
+namespace Ziggurat.Registration.Domain
+{
+    public abstract class ApplicationServiceBase<T>
+        where T : IAggregateRoot, new()
+    {
+        private readonly IEventStore _store;
+
+        protected ApplicationServiceBase(IEventStore store)
+        {
+            if (store == null) throw new ArgumentNullException("store");
+            _store = store;
+        }
+
+        //this.Update(myId, ar => ar.DoSomething(parameters));
+        protected void Update(Guid aggregateId, Action<T> updateAction)
+        {
+            var events = _store.LoadAll(aggregateId);
+
+            var aggregate = new T();
+            aggregate.ApplyFromHistory(events.Events.Cast<IEvent>());
+
+            updateAction(aggregate);
+
+            _store.Append(aggregateId, events.Revision, aggregate.Changes.Select(x=>WrapIntoEnvelope(aggregateId, x)));
+        }
+
+        private Envelope WrapIntoEnvelope(Guid aggregateId, IEvent evt)
+        {
+            var envelope = new Envelope(evt, null);
+            envelope.AggregateId = aggregateId;
+            envelope.DateCreated = Now.UtcTime;
+
+            return envelope;
+        }
+    }
+
+}
