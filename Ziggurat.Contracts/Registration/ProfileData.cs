8cb99d2f351fc655f4881073c54f36ff3fbb8095
Alexey Raga <alexey.raga@gmail.com>
2012-08-26 15:01:27 +1000

    Refine profile data

--- Ziggurat.Contracts/Registration/ProfileData.cs
+++ Ziggurat.Contracts/Registration/ProfileData.cs
@@ -10,5 +10,6 @@ namespace Ziggurat.Contracts.Registration
     {
         public Guid ProfileId { get; set; }
         public string DisplayName { get; set; }
+        public string Email { get; set; }
     }
 }
