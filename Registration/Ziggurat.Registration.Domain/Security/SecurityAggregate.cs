7505cab68fb487081ee732cf320f7d8240fcbcd1
Alexey Raga <alexey.raga@gmail.com>
2012-08-26 16:35:41 +1000

    Security aggregate: create for registration

--- Registration/Ziggurat.Registration.Domain/Security/SecurityAggregate.cs
+++ Registration/Ziggurat.Registration.Domain/Security/SecurityAggregate.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ziggurat.Contracts.Registration;
 
 namespace Ziggurat.Registration.Domain.Security
 {
-    public sealed class SecurityAggregate : AggregateRootBase
+    public sealed class SecurityAggregate : AggregateRootBase<SecurityState>
     {
-        
+        public void CreateForRegistration(Guid id, Guid registrationId, SecurityData data)
+        {
+            Apply(new SecurityCreated(id, data.Login));
+            Apply(new SecurityPasswordSet(id, data.Login, data.Password));
+            Apply(new SecurityCreatedForRegistration(id, registrationId, data.Login, data.Email));
+        }
     }
 }
--- Registration/Ziggurat.Registration.Domain/Security/SecurityState.cs
+++ Registration/Ziggurat.Registration.Domain/Security/SecurityState.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ziggurat.Contracts.Registration;
 
 namespace Ziggurat.Registration.Domain.Security
 {
     public sealed class SecurityState : State
     {
-        public Guid UserId { get; set; }
+        public Guid SecurityId { get; set; }
+        public string Login { get; set; }
 
+        public void When(SecurityCreated evt)
+        {
+            SecurityId = evt.SecurityId;
+            Login = evt.Login;
+        }
     }
 }
