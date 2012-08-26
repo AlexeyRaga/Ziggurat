19b7655e8f327f7473d9327603d0effb7d078424
Alexey Raga <alexey.raga@gmail.com>
2012-08-26 15:50:16 +1000

    Adding a refistration process

---
+++ Registration/Ziggurat.Registration.Domain/Processes/RegistrationProcess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Contracts.Registration;
+using Ziggurat.Infrastructure;
+
+namespace Ziggurat.Registration.Domain.Processes
+{
+    public sealed class RegistrationProcess
+    {
+        private readonly ICommandSender _commandSender;
+        public RegistrationProcess(ICommandSender commandSender)
+        {
+            if (commandSender == null) throw new ArgumentNullException("commandSender");
+            _commandSender = commandSender;
+        }
+
+        public void When(RegistrationCreated evt)
+        {
+        }
+    }
+}
