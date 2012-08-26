8c11dca0027d63a9b18703f6df00b8cae4176a79
Alexey Raga <alexey.raga@gmail.com>
2012-08-26 16:13:51 +1000

    Add missing application service

---
+++ Registration/Ziggurat.Registration.Domain/Security/SecurityApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Infrastructure.EventStore;
+
+namespace Ziggurat.Registration.Domain.Security
+{
+    public sealed class SecurityApplicationService : ApplicationServiceBase<SecurityAggregate>
+    {
+        public SecurityApplicationService(IEventStore eventStore)
+            : base(eventStore)
+        {
+
+        }
+    }
+}
