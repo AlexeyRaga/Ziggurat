8b597fda3ec64ebe90f12db1daf3ff83649da4c0
Alexey Raga <alexey.raga@gmail.com>
2012-08-26 15:13:18 +1000

    Profile aggregate

---
+++ Registration/Ziggurat.Registration.Domain/Profile/ProfileApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Infrastructure.EventStore;
+
+namespace Ziggurat.Registration.Domain.Profile
+{
+    public sealed class ProfileApplicationService : ApplicationServiceBase<ProfileAggregate>
+    {
+        public ProfileApplicationService(IEventStore eventStore)
+            : base(eventStore)
+        {
+
+        }
+    }
+}
