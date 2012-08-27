eb0a52e9935254cac529c6b1e94367eb556b298b
Alexey Raga <alexey.raga@gmail.com>
2012-08-27 23:42:12 +1000

    More unit tests on registration

---
+++ Registration/Ziggurar.Registration.Domain.Tests/Registration/When_attach_profile.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Contracts;
+using Ziggurat.Contracts.Registration;
+using Ziggurat.Infrastructure;
+using Ziggurat.Registration.Domain.Registration;
+using Ziggurat.Registration.Domain.Tests;
+
+namespace Ziggurar.Registration.Domain.Tests.Registration
+{
+    [TestClass]
+    public sealed class When_attach_profile : AggregateTest<RegistrationAggregate>
+    {
+        [TestMethod]
+        public void Should_only_attach_profile()
+        {
+            var createdEvent = GetCreatedEvent();
+
+            Given = new IEvent[] { createdEvent };
+            When = aggregate => aggregate.AttachProfile(createdEvent.Profile.ProfileId);
+            Then = new IEvent[] {
+                new ProfileAttachedToRegistration(createdEvent.RegistrationId, createdEvent.Profile.ProfileId)
+            };
+        }
+
+        [TestMethod]
+        public void Should_attach_profile_and_complete_registration()
+        {
+            var createdEvent = GetCreatedEvent();
+
+            Given = new IEvent[] { 
+                createdEvent,
+                new SecurityAttachedToRegistration(createdEvent.RegistrationId, createdEvent.Security.SecurityId)
+            };
+
+            When = aggregate => aggregate.AttachProfile(createdEvent.Profile.ProfileId);
+            Then = new IEvent[] {
+                new ProfileAttachedToRegistration(createdEvent.RegistrationId, createdEvent.Profile.ProfileId),
+                new RegistrationSucceded(createdEvent.RegistrationId, createdEvent.Security.SecurityId, createdEvent.Profile.ProfileId, createdEvent.Security.Login)
+            };
+        }
+
+        private RegistrationCreated GetCreatedEvent()
+        {
+            var regId = RegistrationIdGenerator.NewRegistrationId();
+
+            var securityId = RegistrationIdGenerator.NewSecutiryId(regId);
+            var profileId = RegistrationIdGenerator.NewProfileId(regId);
+
+            var profile = new ProfileData(profileId, "Alexey Raga", "alexey.raga@somewhere.in");
+            var security = new SecurityData(securityId, "alexeyraga", "alexey.raga@somewhere.in", "Password123");
+
+            return new RegistrationCreated(regId, Now.UtcTime, security, profile);
+        }
+    }
+}
---
+++ Registration/Ziggurar.Registration.Domain.Tests/Registration/When_attach_security.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ziggurat.Contracts;
+using Ziggurat.Contracts.Registration;
+using Ziggurat.Infrastructure;
+using Ziggurat.Registration.Domain.Registration;
+using Ziggurat.Registration.Domain.Tests;
+
+namespace Ziggurar.Registration.Domain.Tests.Registration
+{
+    [TestClass]
+    public sealed class When_attach_security : AggregateTest<RegistrationAggregate>
+    {
+        [TestMethod]
+        public void Should_only_attach_security()
+        {
+            var createdEvent = GetCreatedEvent();
+
+            Given = new IEvent[] { createdEvent };
+            When = aggregate => aggregate.AttachSecurity(createdEvent.Security.SecurityId);
+            Then = new IEvent[] {
+                new SecurityAttachedToRegistration(createdEvent.RegistrationId, createdEvent.Security.SecurityId)
+            };
+        }
+
+        [TestMethod]
+        public void Should_attach_security_and_complete_registration()
+        {
+            var createdEvent = GetCreatedEvent();
+
+            Given = new IEvent[] { 
+                createdEvent,
+                new ProfileAttachedToRegistration(createdEvent.RegistrationId, createdEvent.Profile.ProfileId)
+            };
+
+            When = aggregate => aggregate.AttachSecurity(createdEvent.Security.SecurityId);
+            Then = new IEvent[] {
+                new SecurityAttachedToRegistration(createdEvent.RegistrationId, createdEvent.Security.SecurityId),
+                new RegistrationSucceded(createdEvent.RegistrationId, createdEvent.Security.SecurityId, createdEvent.Profile.ProfileId, createdEvent.Security.Login)
+            };
+        }
+
+        private RegistrationCreated GetCreatedEvent()
+        {
+            var regId = RegistrationIdGenerator.NewRegistrationId();
+
+            var securityId = RegistrationIdGenerator.NewSecutiryId(regId);
+            var profileId = RegistrationIdGenerator.NewProfileId(regId);
+
+            var profile = new ProfileData(profileId, "Alexey Raga", "alexey.raga@somewhere.in");
+            var security = new SecurityData(securityId, "alexeyraga", "alexey.raga@somewhere.in", "Password123");
+
+            return new RegistrationCreated(regId, Now.UtcTime, security, profile);
+        }
+    }
+}
