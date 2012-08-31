cbc20bda2c1684448d7581fabaed71e568c23dcb
Alexey Raga <alexey.raga@gmail.com>
2012-08-31 14:43:34 +1000

    Rename the command sender

---
+++ Infrastructure/Ziggurat.Infrastructure/ToDispatcherCommandSender.cs
@@ -0,0 +1,19 @@
+﻿using Ziggurat.Infrastructure.Evel;
+
+namespace Ziggurat.Infrastructure
+{
+    public sealed class ToDispatcherCommandSender : ICommandSender
+    {
+        private readonly IMessageDispatcher _sender;
+
+        public void SendCommand(object command)
+        {
+            _sender.DispatchToOneAndOnlyOne(command);
+        }
+
+        public ToDispatcherCommandSender(IMessageDispatcher commandSender)
+		{
+            _sender = commandSender;
+		}
+    }
+}
