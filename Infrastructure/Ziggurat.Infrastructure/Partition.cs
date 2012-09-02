37adf06d2e7e93c3f24939a6554055480f94abbd
Alexey Raga <alexey.raga@gmail.com>
2012-09-02 21:08:04 +1000

    Rename paramerer

--- Infrastructure/Ziggurat.Infrastructure/Partition.cs
+++ Infrastructure/Ziggurat.Infrastructure/Partition.cs
@@ -9,11 +9,11 @@ namespace Ziggurat.Infrastructure
 {
     public static class Partition
     {
-        public static byte GetPartition(string login)
+        public static byte GetPartition(string value)
         {
             using (var md = new MD5CryptoServiceProvider())
             {
-                var bytes = Encoding.UTF8.GetBytes(login.ToLowerInvariant());
+                var bytes = Encoding.UTF8.GetBytes(value.ToLowerInvariant());
                 return md.ComputeHash(bytes)[0];
             }
         }
