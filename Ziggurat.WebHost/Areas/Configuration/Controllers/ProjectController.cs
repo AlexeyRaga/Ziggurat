b0d0dd015a844f676a0f4051dcf979faedb86905
Alexey Raga <alexey.raga@gmail.com>
2012-09-07 23:43:45 +1000

    Validate project ShortName uniqueness

--- Ziggurat.WebHost/Areas/Configuration/Controllers/ProjectController.cs
+++ Ziggurat.WebHost/Areas/Configuration/Controllers/ProjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Ziggurat.Contracts.Definition;
+using Ziggurat.Definition.Client;
 using Ziggurat.Infrastructure;
 using Ziggurat.Web.Areas.Configuration.Models;
 
@@ -31,17 +32,29 @@ namespace Ziggurat.Web.Areas.Configuration.Controllers
         [HttpPost]
         public ActionResult New(CreateProjectModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View();
+            if (!ValidateUniqueShortName(model.ShortName))
             {
-                var newProjectId = DefinitionIdGenerator.NewProjectId();
-                var cmd = new CreateProject(newProjectId, model.Name, model.ShortName);
-
-                _commandSender.SendCommand(cmd);
-
-                return View("ProjectIsBeingCreated");
+                ModelState.AddModelError("", String.Format("Short name {0} is already taken.", model.ShortName));
+                return View();
             }
 
-            return View();
+            var newProjectId = DefinitionIdGenerator.NewProjectId();
+            var cmd = new CreateProject(newProjectId, model.Name, model.ShortName);
+
+            _commandSender.SendCommand(cmd);
+
+            return View("ProjectIsBeingCreated");
+
+        }
+
+        private bool ValidateUniqueShortName(string shortName)
+        {
+            var list = _modelReader.LoadOrDefault<string, ProjectList>("all");
+            if (list == null) return true; //no project at all => unique
+
+            return !list.Projects
+                .Any(x => String.Equals(x.ShortName, shortName, StringComparison.InvariantCultureIgnoreCase));
         }
 
         public ActionResult Configure(Guid id)
