9b75e2075701e65a282db9fc96079e76ff72bc36
Alexey Raga <alexey.raga@gmail.com>
2012-09-13 00:06:40 +1000

    Add a form: beginning

---
+++ Ziggurat.WebHost/Areas/Configuration/Controllers/FormController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ziggurat.Contracts.Definition;
+using Ziggurat.Definition.Client;
+using Ziggurat.Infrastructure;
+using Ziggurat.Web.Areas.Configuration.Models;
+using Ziggurat.Web.Helpers;
+
+namespace Ziggurat.Web.Areas.Configuration.Controllers
+{
+    public class FormController : Controller
+    {
+        private IViewModelReader _viewModelReader;
+        private ICommandSender _commandSender;
+        
+        public FormController(IViewModelReader reader, ICommandSender sender)
+        {
+            _viewModelReader = reader;
+            _commandSender = sender;
+        }
+
+        public FormController()
+            : this(Client.ViewModelReader, Client.CommandSender) { }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNewForm(CreateFormModel model)
+        {
+            if (!ModelState.IsValid) return Json("");
+
+            Response.StatusCode = 500;
+            return Json("Invalid something");
+
+            var currentProject = DomainHelper.GetCurrentProjectDomain(Request);
+            var projectInfo = _viewModelReader.LoadOrDefault<string, ProjectInfo>(currentProject);
+
+            if (projectInfo == null) throw new InvalidOperationException("Unknown project");
+
+            var formId = DefinitionIdGenerator.NewFormId();
+
+            var cmd = new CreateForm(projectInfo.ProjectId, formId, model.Name, model.UniqueName);
+
+            return Json("OK");
+        }
+    }
+}
---
+++ Ziggurat.WebHost/Areas/Configuration/Models/CreateFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ziggurat.Web.Areas.Configuration.Models
+{
+    public sealed class CreateFormModel
+    {
+        [Required]
+        [DataType(DataType.Text), MaxLength(50)]
+        [Display(Name = "Form type name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Text), MaxLength(50)]
+        [Display(Name = "Form type unique name")]
+        public string UniqueName { get; set; }
+    }
+}
