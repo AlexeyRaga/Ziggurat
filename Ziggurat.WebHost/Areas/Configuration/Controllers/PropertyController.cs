ac7d9144afe1ceeccae6556488b4d54dac668af2
Alexey Raga <alexey.raga@gmail.com>
2012-09-16 13:20:59 +1000

    Preparing to be able to add properties

---
+++ Ziggurat.WebHost/Areas/Configuration/Controllers/PropertyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ziggurat.Web.Areas.Configuration.Models;
+
+namespace Ziggurat.Web.Areas.Configuration.Controllers
+{
+    [Authorize]
+    public class PropertyController : Controller
+    {
+        [HttpPost]
+        public ActionResult AddNew(NewPropertyModel model)
+        {
+            return Json("");
+        }
+    }
+}
---
+++ Ziggurat.WebHost/Areas/Configuration/Models/NewPropertyModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Ziggurat.Contracts.Definition;
+
+namespace Ziggurat.Web.Areas.Configuration.Models
+{
+    public sealed class NewPropertyModel
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public PropertyType Type { get; set; }
+    }
+}
