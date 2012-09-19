ec13ca387675cdb3b56abff87a9f50d4e9e64aeb
Alexey Raga <alexey.raga@gmail.com>
2012-09-19 15:10:47 +1000

    Fixing a potential bug in doc store

--- Infrastructure/Ziggurat.Infrastructure/DocumentStore/DocumentStoreIOException.cs
+++ Infrastructure/Ziggurat.Infrastructure/DocumentStore/DocumentStoreIOException.cs
@@ -6,20 +6,14 @@ namespace Ziggurat.Infrastructure.DocumentStore
     [Serializable]
     public sealed class DocumentStoreIOException : Exception
     {
-        public object Key
-        {
-            get { return Data.Contains("docKey") ? Data["docKey"] : null; }
-        }
-
         public DocumentStoreIOException() { }
 
         public DocumentStoreIOException(string message)
             : base(message) { }
 
-        public DocumentStoreIOException(object documentKey, string message, Exception inner)
+        public DocumentStoreIOException(string message, Exception inner)
             : base(message, inner)
         {
-            Data.Add("docKey", documentKey);
         }
 
         private DocumentStoreIOException(SerializationInfo info, StreamingContext context)
