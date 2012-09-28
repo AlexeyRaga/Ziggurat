3d1f09587e856b5619a119a24576596b06bda12b
Alexey Raga <alexey.raga@gmail.com>
2012-09-28 22:54:52 +1000

    Concatenation formula view model

--- Ziggurat.WebHost/Areas/Configuration/Scripts/configuration.concatenation.formula.js
+++ Ziggurat.WebHost/Areas/Configuration/Scripts/configuration.concatenation.formula.js
@@ -1,3 +1,19 @@
 ﻿$(function() {
+    function FormulaModel() {
+        var model = this;
 
+        this.parts = ko.observableArray([]);
+        this.newConstantPart = ko.observable();
+        this.newPropertyPart = ko.observable();
+
+        this.addNewPart = function () {
+            var constant = model.newConstantPart();
+            alert(model.newConstantPart());
+        };
+    };
+
+    var view = $('#concatenationFormula')[0];
+    var model = new FormulaModel();
+
+    ko.applyBindings(model, view);
 });
