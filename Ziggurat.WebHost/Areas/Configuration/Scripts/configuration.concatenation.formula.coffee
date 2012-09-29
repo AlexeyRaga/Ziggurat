2443f1525a54f8b7d3988ef83515221fed18ef83
Alexey Raga <alexey.raga@gmail.com>
2012-09-30 00:32:33 +1000

    Concatenation formula UI: kinda working

--- Ziggurat.WebHost/Areas/Configuration/Scripts/configuration.concatenation.formula.coffee
+++ Ziggurat.WebHost/Areas/Configuration/Scripts/configuration.concatenation.formula.coffee
@@ -1,28 +1,35 @@
 ﻿$ ->
-  class FormulaModel
-    model = this
+    class FormulaModel
+        constructor: ->
+            @parts = ko.observableArray()
+            @newConstantPart = ko.observable()
+            @newPropertyPart = ko.observable()
+    
+            @formula = ko.computed =>
+                @parts()
+                    .map((item) -> item.text)
+                    .join('')
+                    
+    
+        addNewPart: ->
+            constant = @newConstantPart()
+            property = @newPropertyPart()
+            if constant? and constant != ''
+              part = { value: constant, text: constant }
+              @parts.push part
+            if property? and property != ''
+              propertyText = $("#propList option[value=#{property}]").text();
+              part = { value: "{{#{property}}}", text: "[#{propertyText}]" }
+              @parts.push part
+    
+            @newConstantPart('')
+            @newPropertyPart('')
+            true
+    
+        removePart: (data) =>
+          @parts.remove data
 
-    parts           : ko.observableArray()
-    formula         : ko.observableArray()	
-    newConstantPart : ko.observable()
-    newPropertyPart : ko.observable()
-    newPropertyName : ko.observable()
+    view = $("#concatenationFormula")[0]
+    model = new FormulaModel()
 
-    addNewPart: =>
-      constant = @newConstantPart()
-      property = @newPropertyPart()
-      if constant? and constant != ''
-        @parts.push constant
-        @formula.push.constant
-      if property? and property != ''
-        propertyText = $('#propList option[value='+ property + ']').text();
-        @formula.push '['+propertyText+']'
-        @parts.push '['+property+']'
-      @newConstantPart('')
-      @newPropertyPart('')
-      true
-
-  view = $("#concatenationFormula")[0]
-  model = new FormulaModel()
-
-  ko.applyBindings model, view
+    ko.applyBindings model, view
--- Ziggurat.WebHost/Areas/Configuration/Scripts/configuration.concatenation.formula.js
+++ Ziggurat.WebHost/Areas/Configuration/Scripts/configuration.concatenation.formula.js
@@ -3,41 +3,47 @@ var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments)
 $(function() {
   var FormulaModel, model, view;
   FormulaModel = (function() {
-    var model;
 
     function FormulaModel() {
-      this.addNewPart = __bind(this.addNewPart, this);
+      this.removePart = __bind(this.removePart, this);
 
+      var _this = this;
+      this.parts = ko.observableArray();
+      this.newConstantPart = ko.observable();
+      this.newPropertyPart = ko.observable();
+      this.formula = ko.computed(function() {
+        return _this.parts().map(function(item) {
+          return item.text;
+        }).join('');
+      });
     }
 
-    model = FormulaModel;
-
-    FormulaModel.prototype.parts = ko.observableArray();
-
-    FormulaModel.prototype.formula = ko.observableArray();
-
-    FormulaModel.prototype.newConstantPart = ko.observable();
-
-    FormulaModel.prototype.newPropertyPart = ko.observable();
-
-    FormulaModel.prototype.newPropertyName = ko.observable();
-
     FormulaModel.prototype.addNewPart = function() {
-      var constant, property, propertyText;
+      var constant, part, property, propertyText;
       constant = this.newConstantPart();
       property = this.newPropertyPart();
       if ((constant != null) && constant !== '') {
-        this.parts.push(constant);
-        this.formula.push.constant;
+        part = {
+          value: constant,
+          text: constant
+        };
+        this.parts.push(part);
       }
       if ((property != null) && property !== '') {
-        propertyText = $('#propList option[value=' + property + ']').text();
-        this.formula.push('[' + propertyText + ']');
-        this.parts.push('[' + property + ']');
+        propertyText = $("#propList option[value=" + property + "]").text();
+        part = {
+          value: "{{" + property + "}}",
+          text: "[" + propertyText + "]"
+        };
+        this.parts.push(part);
       }
       this.newConstantPart('');
       this.newPropertyPart('');
       return true;
+    };
+
+    FormulaModel.prototype.removePart = function(data) {
+      return this.parts.remove(data);
     };
 
     return FormulaModel;
