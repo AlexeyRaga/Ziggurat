27c6c9bf525435cc4a17f4a958e4f79e9cdb9a14
ldsenow <ldsenow@gmail.com>
2012-10-02 08:15:19 +1000

    extracted the base class for routing tests

--- Ziggurat.WebHost.Tests/Routes/ConfigurationAreaRoutingTest.cs
+++ Ziggurat.WebHost.Tests/Routes/ConfigurationAreaRoutingTest.cs
@@ -9,22 +9,11 @@ using Ziggurat.Web.Areas.Configuration;
 namespace Ziggurat.WebHost.Tests.Routes
 {
     [TestClass]
-    public class ConfigurationAreaRoutingTest
+    public class ConfigurationAreaRoutingTest : RoutingTestBase
     {
         [TestMethod]
         public void Configuration_property_incoming_route()
         {
-            //TODO: LEISHI extra to the base
-            var routes = new RouteCollection();
-            //TODO: LEISHI all the areas should be registered otherwise the route testing is kind of useless as 
-            var areaRegistration = new ConfigurationAreaRegistration();
-            Assert.AreEqual("Configuration", areaRegistration.AreaName);
-
-            var areaRegistrationContext = new AreaRegistrationContext(areaRegistration.AreaName, routes);
-            areaRegistration.RegisterArea(areaRegistrationContext);
-
-            RouteConfig.RegisterRoutes(routes);
-
             var formId = "af3c7b50-fadd-407c-b28a-0fab60be264c";
             var propertyId = "2e9939df-4462-4717-a5dc-a174a29d11c1";
 
@@ -32,7 +21,7 @@ namespace Ziggurat.WebHost.Tests.Routes
             context.Setup(c => c.Request.AppRelativeCurrentExecutionFilePath)
                 .Returns("~/configuration/form/" + formId + "/p/" + propertyId);
 
-            var routeData = routes.GetRouteData(context.Object);
+            var routeData = Routes.GetRouteData(context.Object);
 
             Assert.IsNotNull(routeData, "Should have found the route");
             Assert.AreEqual("Configuration", routeData.DataTokens["area"]);
---
+++ Ziggurat.WebHost.Tests/Routes/RoutingTestBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ziggurat.Web;
+using Ziggurat.Web.Areas.Account;
+using Ziggurat.Web.Areas.Configuration;
+using Ziggurat.Web.Areas.Contacts;
+using Ziggurat.Web.Areas.Forms;
+
+namespace Ziggurat.WebHost.Tests.Routes
+{
+    [TestClass]
+    public class RoutingTestBase
+    {
+        protected RouteCollection Routes = new RouteCollection();
+
+        [TestInitialize]
+        public void RegisterRoutes()
+        {
+            var areaRegistrations = GetAllKnownAreas();
+            foreach (var areaRegistration in areaRegistrations)
+            {
+                var areaRegistrationContext = new AreaRegistrationContext(areaRegistration.AreaName, Routes);
+                areaRegistration.RegisterArea(areaRegistrationContext);
+            }
+
+            RouteConfig.RegisterRoutes(Routes);
+        }
+
+        private IEnumerable<AreaRegistration> GetAllKnownAreas()
+        {
+            //NOTE: The order of the area registration is important as the first match first return
+            yield return new AccountAreaRegistration();
+            yield return new ConfigurationAreaRegistration();
+            yield return new ContactsAreaRegistration();
+            yield return new FormsAreaRegistration();
+        }
+    }
+}
